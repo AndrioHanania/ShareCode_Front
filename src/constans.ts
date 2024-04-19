ae6b43783cfcea66a3aaeffc08bacbab813b2242
Andrio Hanania <andrio1hanania@gmail.com>
2024-04-19 15:56:24 +0300

    fix constans file

--- src/constans.ts
+++ src/constans.ts
@@ -1,9 +1,1 @@
-export const serverURL =
-  process.env.NODE_ENV === "production"
-    ? "https://sharecode-back.onrender.com/"
-    : process.env.REACT_APP_SERVER_URL;
-
-console.log(
-  "process.env.REACT_APP_SERVER_URL: " + process.env.REACT_APP_SERVER_URL
-);
-console.log("process.env.NODE_ENV: " + process.env.NODE_ENV);
+export const serverURL = process.env.REACT_APP_SERVER_URL;
